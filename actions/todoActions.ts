349674507470ef21f677efd746c434cc82e93280
Firas Ben Hmida <127399137+firas-ben-hmida@users.noreply.github.com>
2025-07-27 21:54:44 +0100

    Update todoActions.ts

--- actions/todoActions.ts
+++ actions/todoActions.ts
@@ -6,18 +6,22 @@ import { db } from "@/db/drizzle";
 import { todos } from "@/db/schema";
 
 export const getData = async (userId: number) => {
-  // const data = await db.select().from(todos);
   const data = await db.select().from(todos).where(eq(todos?.userId, userId));
   return data;
 };
 
 export const addTodo = async (id: number, text: string, userId: number) => {
-  await db.insert(todos).values({
-    id,
-    text: text,
-    userId,
-  });
-  revalidatePath("/");
+  try {
+    await db.insert(todos).values({
+      id,
+      text: text,
+      userId,
+    });
+    revalidatePath("/");
+  } catch (error) {
+    console.error('Erreur lors de l\'ajout du todo:', error);
+    throw error;
+  }
 };
 
 export const deleteTodo = async (id: number) => {
